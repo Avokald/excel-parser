994a4d2c3b3d42712350ecc0e3ee93ed27ae0588
avokald <baldykov_a@yahoo.com>
2020-11-26 15:45:52 +0500

    Change route name for frontpage

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,7 +2,7 @@
     <nav>
         <ul class="nav">
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('index') }}">Таблица</a>
+                <a class="nav-link" href="{{ route('frontpage') }}">Таблица</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('upload') }}">Загрузить файл</a>
